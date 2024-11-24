ae0ce3c07f2391370fb1495a50b13b881f89970e
LiceeIF <a.alicemaria.oliveira@gmail.com>
2024-11-24 14:56:56 -0300

    updating a piece

--- flowvis/app.py
+++ flowvis/app.py
@@ -14,4 +14,47 @@ def cadastro():
 def login():
     return render_template("inicio.html")
 
+# acho que tem que arrumar aqui:
+# @app.route("/")
+# def recover_password_page():
+#     return render_template("recover_password.html")
+
+# @app.route("/recover_password", methods=["POST"])
+# def recover_password():
+#     email = request.form.get("email")
+#     # Aqui você pode adicionar lógica para envio de e-mail com token de recuperação
+#     print(f"Instruções enviadas para: {email}")
+#     return redirect(url_for("update_password_page"))
+
+# @app.route("/update_password", methods=["GET", "POST"])
+# def update_password_page():
+#     if request.method == "POST":
+#         new_password = request.form.get("new_password")
+#         confirm_password = request.form.get("confirm_password")
+#         if new_password == confirm_password:
+#             print(f"Senha alterada com sucesso: {new_password}")
+#             return "Senha alterada com sucesso!"
+#         else:
+#             return "As senhas não coincidem, tente novamente."
+#   return render_template("recover_password_step2.html")
+#
+
+# @app.route("/")
+# def profile():
+#     # Informações dinâmicas podem ser passadas para o template aqui
+#     user_data = {
+#         "name": "Alice Maria",
+#         "username": "@licemaria",
+#         "following": 28,
+#         "followers": 168,
+#         "suggestions": [
+#             {"name": "Alina Boz", "location": "Santa Monica, CA", "username": "@BozAlina", "image": "user1.jpg"},
+#             {"name": "Elijah Mikaelson", "location": "Sófia, BG", "username": "@elijahmikael", "image": "user2.jpg"}
+#         ],
+#         "events": [
+#             {"title": "AUTÓDROMO DE INTERLAGOS", "date": "NOVEMBRO 14", "description": "Evento: I-N-C-R-Í-V-E-L", "tags": "#race #formula1", "image": "race.jpg"}
+#         ]
+#     }
+#     return render_template("profile.html", user=user_data)
+
 app.run(port=5001)
