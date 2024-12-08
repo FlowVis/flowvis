032cbc03d321da2d974900f7daf5fd84da55835e
Rodrigo <rodrigobaesa@gmail.com>
2024-12-08 18:50:46 -0300

    login sign error handling

--- flowvis/app.py
+++ flowvis/app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, redirect, render_template, request, session, url_for
 import hashlib
 import mysql.connector
@@ -14,6 +15,23 @@ def conexao_abrir():
         database="flowvis"
         )
 
+def verificar_usuario_existente(email, username):
+    con = conexao_abrir()
+    cursor = con.cursor(dictionary=True)
+    
+    query_email = "SELECT * FROM usuario WHERE usuario_email = %s"
+    cursor.execute(query_email, (email,))
+    email_existente = cursor.fetchone()
+
+    query_username = "SELECT * FROM usuario WHERE usuario_user = %s"
+    cursor.execute(query_username, (username,))
+    username_existente = cursor.fetchone()
+
+    cursor.close()
+    con.close()
+
+    return email_existente, username_existente
+
 def cadastrar_usuario(u):
     con = conexao_abrir()
     cursor = con.cursor()
@@ -28,12 +46,48 @@ def cadastrar_usuario(u):
     con.close()
 
 @app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if validar_usuario(email, password):
+            return redirect(url_for("home"))
+        else:
+            return redirect(url_for("login"))
+    return render_template("login.html")
+
+@app.route("/signin", methods=["GET", "POST"])
 def signin():
     if request.method == "POST":
         nome = request.form.get("name")
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
+
+        email_existente, username_existente = verificar_usuario_existente(email, username)
+
+        if email_existente:
+            return render_template("signin.html", error="O e-mail já está cadastrado.")
+        
+        if username_existente:
+            return render_template("signin.html", error="O nome de usuário já está em uso.")
+
+        password_validation = password_check(password)
+        if not password_validation['password_ok']:
+            error_messages = []
+            if password_validation['length_error']:
+                error_messages.append("A senha deve ter pelo menos 8 caracteres.")
+            if password_validation['digit_error']:
+                error_messages.append("A senha deve conter pelo menos um dígito.")
+            if password_validation['uppercase_error']:
+                error_messages.append("A senha deve conter pelo menos uma letra maiúscula.")
+            if password_validation['lowercase_error']:
+                error_messages.append("A senha deve conter pelo menos uma letra minúscula.")
+            if password_validation['symbol_error']:
+                error_messages.append("A senha deve conter pelo menos um símbolo.")
+
+            return render_template("signin.html", error="<br>".join(error_messages))
+
         cadastrar_usuario({
             "usuario_nome": nome,
             "usuario_user": username,
@@ -47,10 +101,6 @@ def signin():
 @app.route("/home")
 def home():
     return render_template("home.html")
-
-@app.route("/forgot-password-email")
-def forgot_password():
-    return render_template("forgotpassword1.html")
 
 def validar_usuario(email, password):
     con = conexao_abrir()
@@ -68,66 +118,37 @@ def validar_usuario(email, password):
         return True
     return False
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        email = request.form.get("email")
-        password = request.form.get("password")
-        if validar_usuario(email, password):
-            return redirect(url_for("home"))
-        else:
-            return redirect(url_for("login"))
-    return render_template("login.html")
-
-@app.route("/recuperar")
-def recuperar_senha():
-    return render_template("senha-redefinir-senha.html")
-
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
 
-# acho que tem que arrumar aqui:
-# @app.route("/")
-# def recover_password_page():
-#     return render_template("recover_password.html")
+####################
+def password_check(password):
+    """
+    Verify the strength of 'password'
+    Returns a dict indicating the wrong criteria
+    """
 
-# @app.route("/recover_password", methods=["POST"])
-# def recover_password():
-#     email = request.form.get("email")
-#     # Aqui você pode adicionar lógica para envio de e-mail com token de recuperação
-#     print(f"Instruções enviadas para: {email}")
-#     return redirect(url_for("update_password_page"))
+    length_error = len(password) < 8
 
-# @app.route("/update_password", methods=["GET", "POST"])
-# def update_password_page():
-#     if request.method == "POST":
-#         new_password = request.form.get("new_password")
-#         confirm_password = request.form.get("confirm_password")
-#         if new_password == confirm_password:
-#             print(f"Senha alterada com sucesso: {new_password}")
-#             return "Senha alterada com sucesso!"
-#         else:
-#             return "As senhas não coincidem, tente novamente."
-#   return render_template("recover_password_step2.html")
-#
+    digit_error = re.search(r"\d", password) is None
 
-# @app.route("/")
-# def profile():
-#     # Informações dinâmicas podem ser passadas para o template aqui
-#     user_data = {
-#         "name": "Alice Maria",
-#         "username": "@licemaria",
-#         "following": 28,
-#         "followers": 168,
-#         "suggestions": [
-#             {"name": "Alina Boz", "location": "Santa Monica, CA", "username": "@BozAlina", "image": "user1.jpg"},
-#             {"name": "Elijah Mikaelson", "location": "Sófia, BG", "username": "@elijahmikael", "image": "user2.jpg"}
-#         ],
-#         "events": [
-#             {"title": "AUTÓDROMO DE INTERLAGOS", "date": "NOVEMBRO 14", "description": "Evento: I-N-C-R-Í-V-E-L", "tags": "#race #formula1", "image": "race.jpg"}
-#         ]
-#     }
-#     return render_template("profile.html", user=user_data)
+    uppercase_error = re.search(r"[A-Z]", password) is None
+
+    lowercase_error = re.search(r"[a-z]", password) is None
+
+    symbol_error = re.search(r"[ @!#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
+
+    password_ok = not (length_error or digit_error or uppercase_error or lowercase_error or symbol_error)
+
+    return {
+        'password_ok': password_ok,
+        'length_error': length_error,
+        'digit_error': digit_error,
+        'uppercase_error': uppercase_error,
+        'lowercase_error': lowercase_error,
+        'symbol_error': symbol_error,
+    }
+
 
 app.run(port=5001)
